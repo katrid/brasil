b51abb9ee09b39d99b440a6f4fcc88dd5e1bea14
alexandredias <alexandre@katrid.com>
2024-07-16 10:15:29 -0300

    atualizacao python 3.12

--- brasil/dfe/cte/v400.py
+++ brasil/dfe/cte/v400.py
@@ -2,7 +2,23 @@ import os
 
 # schemas não constantes na versão 4.00
 import brasil.dfe.leiaute.cte.cte_v400
+from brasil.dfe.leiaute.cte.procCTe_v400 import cteProc
+from brasil.dfe.leiaute.cte.retConsStatServCTe_v400 import retConsStatServCTe
+from brasil.dfe.leiaute.cte.consSitCTe_v400 import consSitCTe
+from brasil.dfe.leiaute.cte.retConsSitCTe_v400 import retConsSitCTe
+from brasil.dfe.leiaute.cte.consStatServCTe_v400 import consStatServCTe
+from brasil.dfe.leiaute.cte.enviCTe_v300 import enviCTe
+from brasil.dfe.leiaute.cte.retEnviCTe_v300 import retEnviCte
+from brasil.dfe.leiaute.cte.eventoCTe_v400 import eventoCTe
+from brasil.dfe.leiaute.cte.retEventoCTe_v400 import retEventoCTe
+from brasil.dfe.leiaute.cte.distDFeInt_v100 import distDFeInt
+from brasil.dfe.leiaute.cte.retDistDFeInt_v100 import retDistDFeInt
+from brasil.dfe.leiaute.cte.consReciCTe_v300 import consReciCTe
+from brasil.dfe.leiaute.cte.retConsReciCTe_v300 import retConsReciCTe
+from brasil.dfe.leiaute.cte.retCTe_v400 import retCTe
+from brasil.dfe.leiaute.cte.evCancCTe_v400 import evCancCTe
 from brasil.dfe.leiaute.cte.cteModalRodoviario_v400 import rodo
+from brasil.utils.text import remover_acentos
 
 
 class CTe(brasil.dfe.leiaute.cte.cte_v400.CTe):
