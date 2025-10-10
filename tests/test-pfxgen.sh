733d1da7360ebf6419b606c352c96d0ce56a441e
alexandredias <alexandre@katrid.com>
2025-10-10 16:03:47 -0300

    atualizacao python3.13

--- tests/test-pfxgen.sh
+++ tests/test-pfxgen.sh
@@ -1,4 +1,4 @@
 # generate pem file
-sudo openssl req -x509 -nodes -days 3650 -newkey rsa:2048 -keyout brasil-test.key -out brasil-test.crt
+openssl req -x509 -nodes -days 3650 -newkey rsa:2048 -keyout test.key -out test.crt
 # generate pfx file
-openssl pkcs12 -export -out brasil-test.pfx -inkey brasil-test.key -in brasil-test.crt
+openssl pkcs12 -export -out test.pfx -inkey test.key -in test.crt
