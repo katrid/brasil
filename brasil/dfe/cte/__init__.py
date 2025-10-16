6235c0d3844c9ece94a216937725ce1b5c30bf89
alexandredias <alexandre@katrid.com>
2025-10-16 18:08:12 -0300

    implementacao ctesimp

--- brasil/dfe/cte/__init__.py
+++ brasil/dfe/cte/__init__.py
@@ -11,3 +11,13 @@ def merge_prot(xml: str, prot: str):
         xml = xml.decode('utf-8')
     prot = lxml.etree.tostring(lxml.etree.fromstring(prot).find('.//{http://www.portalfiscal.inf.br/cte}protCTe')).decode('utf-8')
     return '<cteProc versao="4.00" xmlns="http://www.portalfiscal.inf.br/cte">' + xml + prot + '</cteProc>'
+
+
+def merge_simp_prot(xml: str, prot: str):
+    cte = lxml.etree.fromstring(xml)
+    if cte.tag == '{http://www.portalfiscal.inf.br/cte}cteSimpProc':
+        xml = lxml.etree.tostring(cte.find('.//{http://www.portalfiscal.inf.br/cte}CTe'))
+    if isinstance(xml, bytes):
+        xml = xml.decode('utf-8')
+    prot = lxml.etree.tostring(lxml.etree.fromstring(prot).find('.//{http://www.portalfiscal.inf.br/cte}protCTe')).decode('utf-8')
+    return '<cteSimpProc versao="4.00" xmlns="http://www.portalfiscal.inf.br/cte">' + xml + prot + '</cteSimpProc>'
